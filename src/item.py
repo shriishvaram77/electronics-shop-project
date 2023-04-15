409c65fb9bef5155a303f3f94c8f56dce204c5bf
GorbaI01 <i.gorbatenco@gmail.com>
2023-04-15 01:58:11 -0400

    homework - 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+from src.settings import CSV_FILE
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,9 +17,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """Возвращает название товара"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Проверяет, если длина наименования товара превышает 10 символов"""
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise ValueError("Exception: Длина наименования товара превышает 10 символов.")
 
     def calculate_total_price(self) -> float:
         """
@@ -23,17 +41,30 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        for item in Item.all:
-            total_price = self.price * self.quantity
-            return total_price
+        goods_price = self.price * self.quantity
+        return goods_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        for item in Item.all:
-            if Item.pay_rate == 1.0:
-                discounted_cost = self.calculate_total_price()
+        self.price *= self.pay_rate
 
-            else:
-                discounted_cost = Item.pay_rate * self.price * self.quantity
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+        with open(CSV_FILE, encoding="windows-1251") as file:
+            reader = csv.DictReader(file)
+            for i in reader:
+                if list(i.keys()) == ['name', 'price', 'quantity']:
+                    cls(i['name'], i['price'], i['quantity'])
+
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        """Cтатический метод, возвращающий число из числа-строки
+        :return: Число в нужном нам формате
+        """
+        return int(number.split(".")[0])
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
+
+from _pytest.python_api import raises
+
 from src.item import Item
+
 
 @pytest.fixture
 def item1():
@@ -15,3 +19,18 @@ def test_apply_discount(item1):
     item1.pay_rate = 0.5
     item1.apply_discount()
     assert item1.price == 5000.0
+
+
+def test_instantiate_from_csv():
+    assert type(Item.all) is list
+    Item.all.clear()
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5.5') == 5
+    with raises(Exception):
+        Item.string_to_number('five')
+    assert Item.string_to_number('6') == 6
+    assert Item.string_to_number('9.8') == 9
